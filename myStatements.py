f76afbabca01a149d54f7b017a9f1e73154cc06e
Alabi, Tobi Michael <56853529+Michael-HK@users.noreply.github.com>
2023-07-18 12:50:26 +0000

    add files to repo

---
+++ myStatements.py
@@ -0,0 +1,9 @@
+my_list = ["Apple", "Cherry", "Lemon"]
+
+my_dict = {"drink": "coffee", "milk": "skimmed"}
+
+for item in my_list:
+    print(f"my favorite fruit is {item}")
+
+for _, value in my_dict.items():
+    print(f"my favorite drink is {value}")
